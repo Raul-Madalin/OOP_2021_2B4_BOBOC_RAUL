375f0b4a7e4ba48acd62ca9f7a355db3a792ed4b
Raul-Madalin <raul05madalin@yahoo.com>
2021-02-22 20:54:31 +0200

    solved

--- Lab1-2/Source.cpp
+++ Lab1-2/Source.cpp
@@ -1,3 +1,34 @@
 //
 // Laboratorul 1 - ex 2
 //
+
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <cstring>
+
+int main()
+{
+	FILE* data = fopen("in.txt", "r");
+
+	char line[100];
+	int lenght;
+	int sum = 0;
+	int power;
+
+	while (fgets(line, 100, data))
+	{
+		lenght = strlen(line);
+		if (line[lenght - 1] == '\n') lenght--;
+
+		power = 1;
+		lenght--;
+		while (lenght >= 0)
+		{
+			sum += power * (line[lenght] - 48);
+			power *= 10;
+			lenght--;
+		}
+	}
+	printf("%d", sum);
+	return 0;
+}
